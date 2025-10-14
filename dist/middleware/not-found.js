6c7ae24121f7791686982df16fdc8458b3df84eb
Tetiana Klitna <klitna.tetiana@gmail.com>
2025-10-14 14:00:42 -0700

    Remove dist folder from repository

--- dist/middleware/not-found.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const http_status_codes_1 = require("http-status-codes");
-const notFound = (req, res) => {
-    return res
-        .status(http_status_codes_1.StatusCodes.NOT_FOUND)
-        .json({ success: false, msg: http_status_codes_1.ReasonPhrases.NOT_FOUND });
-};
-exports.default = notFound;
