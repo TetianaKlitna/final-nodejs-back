0f39699fc21925cac013e426762e0c307a0febaa
Tetiana Klitna <klitna.tetiana@gmail.com>
2025-10-15 10:56:09 -0700

    resend

--- src/service/mail-service.ts
+++ src/service/mail-service.ts
@@ -1,44 +1,31 @@
 import nodemailer, { Transporter } from 'nodemailer'
 import SMTPTransport from 'nodemailer/lib/smtp-transport'
-import dns from 'node:dns'
-dns.setDefaultResultOrder?.('ipv4first')
+import { Resend } from 'resend'
+
 class MailService {
-  transporter: Transporter<SMTPTransport.SentMessageInfo, SMTPTransport.Options>
+  //transporter: Transporter<SMTPTransport.SentMessageInfo, SMTPTransport.Options>
 
-  constructor () {
-    const options: SMTPTransport.Options = {
-      host: process.env.SMTP_HOST,
-      port: Number(process.env.SMTP_PORT) || 587,
-      secure: false,
-      requireTLS: true,
-      auth: {
-        user: process.env.SMTP_USER,
-        pass: process.env.SMTP_PASSWORD
-      },
-      connectionTimeout: 15_000, // 15 seconds
-      greetingTimeout: 10_000,
-      socketTimeout: 20_000,
-      tls: { minVersion: 'TLSv1.2', servername: process.env.SMTP_HOST }
-    }
+  //constructor () {
+  // const options: SMTPTransport.Options = {
+  //   host: process.env.SMTP_HOST,
+  //   port: Number(process.env.SMTP_PORT) || 587,
+  //   secure: false,
+  //   auth: {
+  //     user: process.env.SMTP_USER,
+  //     pass: process.env.SMTP_PASSWORD
+  //   }
+  // }
 
-    this.transporter = nodemailer.createTransport(options)
-  }
+  // this.transporter = nodemailer.createTransport(options)
+  //}
+
+  resend = new Resend(process.env.RESEND_API_KEY)
 
   async sendActivationLink (to: string, link: string): Promise<void> {
-    await this.transporter.sendMail({
+    await this.resend.emails.send({
       from: `"NextTask Team" <${process.env.SMTP_USER}>`,
       to,
       subject: 'Welcome! Please activate your account',
-      text: `Hello, 
-        Thank you for signing up for NextTask! 
-        To get started, please activate your account by clicking the link below:
-
-        ${link}
-
-        If you did not create this account, please ignore this email.  
-
-        Best regards,  
-        NextTask Team`,
       html: `
             <p>Hello,</p>
             <p>Thank you for signing up for <strong>NextTask</strong>! 🎉</p>
@@ -52,20 +39,10 @@ class MailService {
   }
 
   async sendResetPasswordLink (to: string, link: string): Promise<void> {
-    await this.transporter.sendMail({
+    await this.resend.emails.send({
       from: `"NextTask Team" <${process.env.SMTP_USER}>`,
       to,
       subject: 'Password Reset Request',
-      text: `Hello, 
-            We received a request to reset your password. 
-            You can reset it by clicking the link below:
-
-            ${link}
-
-            If you did not request a password reset, please ignore this message. 
-
-            Best regards,  
-            NextTask Team`,
       html: `
                 <p>Hello,</p>
                 <p>We received a request to reset your password.</p>
